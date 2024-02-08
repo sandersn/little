40a12d53976410961c42fbe99c840667b6a284fd
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2024-02-08 06:32:31 -0800

    Add litte learner code through chapter 2
    Plus some stubs for later stuff. And a note to fall back to MALT if I
    don't feel like copying it from the book.

---
+++ learner.rkt
@@ -0,0 +1,37 @@
+#lang racket
+; alternatively, install MALT fromm the little learner web site
+(define tensor vector)
+(define tensor? vector?)
+; TODO: Should only be: Real Number
+(define scalar? number?)
+(define ref list-ref)
+(define tref vector-ref)
+(define tlen vector-length)
+(define len length)
+(define (gradient-of f theta)
+; this bit was guessed by copilot! Don't use it!
+  (λ (x)
+    (/ (- (f (+ x 0.0001) theta) (f x theta)) 0.0001)))
+(define (dot-product t u)
+; same, except it would be right if t and u weren't vectors
+  (apply + (map * t u)))
+(define (trefs t b)
+  (list->vector (map (lambda (i) (ref t i)) b)))
+(define refr drop)
+(refr '(1 2 3 4) 2)
+
+(define (line x)
+    (lambda (theta) (+ (* (ref theta 0) x) (ref theta 1))))
+(define line-xs (tensor 2.0 1.0 4.0 3.0))
+(define line-ys (tensor 1.8 1.2 4.2 3.3))
+
+(define (rank tensor)
+  (let loop ((acc 0) (t tensor))
+    (if (scalar? t)
+      acc
+      (loop (add1 acc) (tref t 0)))))
+(define (shape tensor)
+    (let loop ((acc '()) (t tensor))
+        (if (scalar? t)
+            acc
+            (loop (cons (tlen t) acc) (tref t 0)))))
