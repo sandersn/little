b3a92c44976f707370abc3cf6525dd4c76f77dd8
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2024-11-23 14:50:04 -0800

    slightly more code in typer.rkt

--- typer.rkt
+++ typer.rkt
@@ -1,4 +1,12 @@
 #lang pie
-TODO
-(display (car 'spinach))
+(claim jumbo Nat)
+(define jumbo 42)
+(claim length (Pi ((T U) (xs (List T))) Nat))
+(define length
+  (lambda (T xs)
+    (rec-List xs
+      0
+      (lambda (x xs length-1)
+        (add1 length-1)))))
+
 ; (car 'spinach)
